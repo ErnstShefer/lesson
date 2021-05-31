016284484c39832d1e7c567d32a31a01a35425c9
Ernst <sheferernst@gmail.com>
2021-05-31 23:49:46 +0500

    Добавил ветку

--- script/script.js
+++ script/script.js
@@ -1,8 +1,16 @@
-const money = 1;
-const income = 2;
-const addExpenses = 3;
-const deposir = 4;
-const mission = 5;
+const money = 700;
+const income = "Frilance";
+const addExpenses = "Интренет, Телефон, Бензин";
+const deposit = true;
+const mission = 2000;
+const period  = 12;
 
-alert("Тестовый режим");
-console.log("Hello World");
+
+console.log(money, income, deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' +  period  + ' месяцев' + ' Цель заработать ' + mission  + ' Рублей' );
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split( ' ' ));
+
+const budgetDay = money;
+console.log(budgetDay);
