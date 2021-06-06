0b122b7d374bd31d3c9def8f481e521ea3820e32
Ernst <sheferernst@gmail.com>
2021-06-06 09:33:08 +0500

    добавил код

--- script/script.js
+++ script/script.js
@@ -1,35 +1,35 @@
 'use strict';
-const  income = 'Frilance',
-       mission = 500000;
-const money = +prompt('Ваш месячный доход?', '100'),
-      addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит'),
-      deposit = confirm('Есть ли у вас депозит в банке?', 'Нет'),
-      expenses1 = prompt('Введите обязательную статью расходов?', 'Квартплата'),
-      amount1 = +prompt('Во сколько это обойдется?', '5000'),
-      expenses2 = prompt('Введите обязательную статью расходов?', 'Проездной'),
-      amount2 = +prompt('Во сколько это обойдется?', '1000');
 
-const budgetMonth = money - amount1 - amount2,
-      budgetDay = Math.floor(budgetMonth / 30),
-      period = Math.ceil(mission / budgetMonth);
+let week = [];
+week['ru'] = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+week['en'] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let lang = prompt('Введите ru для русского, en для английского', 'ru');
+let namePerson = prompt('Введите имя Артем или Максим', 'Артем');
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-console.log(addExpenses.toLowerCase().split(', '));
-console.log(`Бюджет на месяц: ${budgetMonth}`);
-console.log(`Бюджет на день: ${budgetDay}`);
-console.log(`Цель будет достигнута за ${period} месяцев(-а)`);
+const map = new Map([['en', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']], 
+                    ['ru', ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']]]);
 
-if(budgetDay >= 1200){
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay < 1200 && budgetDay >=600){
-    console.log('У вас средний уровень дохода');
-}  else if (budgetDay < 600 && budgetDay >=0){
-    console.log('К сожалению у вас уровень дохода ниже среднего');
+if (lang === 'ru') {
+    console.log(week['ru']);
+} else if (lang === 'en') {
+    console.log(week['en']);
 } else {
     console.log('Что то пошло не так');
 }
+
+switch(lang) {
+	case ('ru'): 
+        console.log(week['ru']);
+    break;
+	case ('en'): 
+        console.log(week['en']);
+    break;
+	default: 
+        console.log('Что то пошло не так');
+}
+
+console.log(week[lang]);
+
+console.log(map.get(lang));
+
+let name = (namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ?  console.log('преподаватель') : console.log('студент');
