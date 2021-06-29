1c55c3dc26c4fcbaeb3ad329ce4eff42e27baebd
Ernst <sheferernst@gmail.com>
2021-06-29 23:45:17 +0500

    Сделал ToDo

---
+++ js/script.js
@@ -0,0 +1,69 @@
+'use strcit';
+
+const  todoControl = document.querySelector('.todo-control'),
+       headerInput = document.querySelector('.header-input'),
+       todoList = document.querySelector('.todo-list'),
+       todoCompleted = document.querySelector('.todo-completed');
+
+const todoData = [] || JSON.parse(localStorage.getItem('data'));      
+
+
+const render = function(){
+
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+    headerInput.value = '';
+
+    todoData.forEach(function(item, i){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+        '<div class="todo-buttons">' +
+        '<button class="todo-remove"></button>' + 
+        '<button class="todo-complete"></button>' + 
+        '</div>';
+
+        if(item.completed){
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        const btnTodoComplete = li.querySelector('.todo-complete');
+        btnTodoComplete.addEventListener('click', function(){
+            item.completed = !item.completed;
+            render();
+        });
+
+        const btnTodoRemove = li.querySelector('.todo-remove');
+        btnTodoRemove.addEventListener('click', function(){
+            todoData.splice(i, 1);
+            li.remove();
+            render();
+        });
+
+        localStorage.setItem('data', JSON.stringify(todoData));
+        
+    });
+};
+
+
+todoControl.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const newTodo = {
+        value: headerInput.value,
+        completed: false
+    };
+
+    if(headerInput.value.trim() === ''){
+        alert('Пусто!');
+      } else {
+        todoData.push(newTodo);
+      }
+
+
+    render();
+})
+render();
